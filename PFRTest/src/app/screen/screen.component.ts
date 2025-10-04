28273a8a33f141d9811c74e81e9a7d0d5c94e553
KyraZhangPFR <kyra.zhang@plantandfood.co.nz>
2025-10-04 23:39:40 +0000

    Display the posts on sidebar

--- PFRTest/src/app/screen/screen.component.ts
+++ PFRTest/src/app/screen/screen.component.ts
@@ -10,20 +10,26 @@ export class ScreenComponent implements OnInit {
   users: any;
   userPosts: any;
   postSelected: any;
+  selectedUser: any;
 
   constructor(private http : HttpClient,) { }
 
   ngOnInit(): void {
     // Get all users information
     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(Response => {
-    this.users = Response;
-  })
+      this.users = Response;
+    });
   }
+  
   // Function to get all user's posts
-  navToUserPost(id){
+  navToUserPost(id) {
+    // Find and store the selected user
+    this.selectedUser = this.users.find(user => user.id === id);
+    // Get user's posts
     this.http.get('https://jsonplaceholder.typicode.com/users/'+ id+ '/posts').subscribe(postResponse => {
       this.userPosts = postResponse;
-    })
+      this.postSelected = null; // Reset selected post when changing user
+    });
   }
   // Get post content by the title selected
   postContent(post){
